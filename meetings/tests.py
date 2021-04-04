843cd5300810c370e7c2b179faf31b941bead7c2
Dinkymoo <daniellefunnygraan@gmail.com>
2021-04-04 15:51:52 +0200

    Add some more properties to the Meeting model

--- meetings/tests.py
+++ meetings/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from meetings.models import Meeting
+from datetime import datetime
 
-# Create your tests here.
+
+class MeetingTestCase(TestCase):
+    def setUp(self):
+        self.date = datetime.now()
+        Meeting.objects.create(title="meeting 1", date=self.date)
+
+    def test_meeting_date_is_set(self):
+        """"Meeting date is set"""
+        meeting = Meeting.objects.get(title="meeting 1")
+        self.assertEqual(meeting.date, self.date.date())
