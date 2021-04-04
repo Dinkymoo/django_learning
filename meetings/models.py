5fdea72cfc77fd919f72451beaddab9f4be844a2
Dinkymoo <daniellefunnygraan@gmail.com>
2021-04-04 16:19:40 +0200

    Update DB to include a room table

--- meetings/models.py
+++ meetings/models.py
@@ -2,14 +2,22 @@ from django.db import models
 from datetime import time
 
 
+class Room(models.Model):
+    name = models.CharField(max_length=50)
+    floor = models.IntegerField()
+    room_number = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.name} floor {self.floor} room_number {self.room_number}"
+
+
 # Create your models here.
 class Meeting(models.Model):
     title = models.CharField(max_length=200)
     date = models.DateField()
     start_time = models.TimeField(default=time(9))
     duration = models.IntegerField(default=1)
-    floor_number = models.IntegerField(default=1)
-    room_number = models.IntegerField(default=1)
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
 
     def __str__(self):
         return f"{self.title} at {self.start_time} on {self.date}"
