dabe9e8ca12111bb421da6a8d8c4767bc475b089
Dinkymoo <daniellefunnygraan@gmail.com>
2021-04-06 13:11:54 +0200

    Add validation

--- meetings/views.py
+++ meetings/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 
 from meetings.models import Meeting, Room
 from django.forms import modelform_factory
@@ -19,4 +19,12 @@ form = MeetingForm()
 
 
 def new(request):
+    if request.method == "POST":
+        form = MeetingForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("home")
+
+    else:
+        form = MeetingForm()
     return render(request, "meetings/new.html", {"form": form})
